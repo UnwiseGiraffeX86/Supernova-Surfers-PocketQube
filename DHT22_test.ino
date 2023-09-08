5c620a174bbaa4b022c5ac60b712f79d4731ec0b
UnwiseGiraffeX86 <stefanut.geala@gmail.com>
2023-09-08 21:47:25 +0100

    DHT22 Test File

---
+++ DHT22_test.ino
@@ -0,0 +1,31 @@
+#include <Wire.h>
+#include <DHT.h>
+
+#define DHTPIN 2 // D2
+#define DHTTYPE DHT22
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup()
+{
+  Serial.begin(115200);
+  Wire.begin();
+  delay(10000);
+
+  dht.begin();
+}
+
+void loop()
+{
+  float dht_temperature, humidity;
+
+  dht_temperature = dht.readTemperature();
+  humidity = dht.readHumidity();
+
+  // Displaying DHT22 Readings
+  Serial.println("======== DHT22 Readings ========");
+  Serial.print("Temperature: "); Serial.println(dht_temperature);
+  Serial.print("Humidity: "); Serial.println(humidity);
+
+  delay(1000);
+}
